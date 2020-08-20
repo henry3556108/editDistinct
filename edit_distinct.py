d6d9574c47bd8329ea18779387452f159e4c3dff
105590003 <henry3556108@gmail.com>
2020-08-20 14:35:05 +0800

    Modify infinite recurtion

--- edit_distinct.py
+++ edit_distinct.py
@@ -3,59 +3,32 @@ import numpy as np
 
 class EditDistinct():
     def __init__(self):
-        self.table = None
+        self.distinct = 0
 
-    def __init_table(self, target: str, compared: str) -> None:
-        '''
-        if you want to get table\n
-        you need to call this function before you calculate\n
-        put strings that you want to comapred to init
-        '''
-        self.table = np.zeros((len(target)+1, len(compared)+1))
-
-    def __minimize(self, a: int, b: int, c: int) -> int:
+    def __minimize(self, a, b, c):
         arr = np.array([a, b, c], dtype="int64")
         return min(arr)
 
-    def get_table(self) -> np.array:
+    def evaluate(self, s1: str, s2: str, step: int = 0) -> int:
         '''
-        return the minimize table back
-        '''
-        return self.table
-
-    def __calculate(self, target: str, compared: str, x: int, y: int) -> int:
-        '''
-        target, compared => string\n
-        x, y => len of target and len of compared\n
+        s1, s2 => string\n
         will return these two string's Edit Distinct back
         '''
-        if x == 0:
-            if type(self.table) == np.ndarray:
-                self.table[x][y] = y
-            return y
-        if y == 0:
-            if type(self.table) == np.ndarray:
-                self.table[x][y] = x
-            return x
+        if len(s1) == 0:
+            step += len(s2)
+            return step
 
-        if target[x - 1] == compared[y - 1]:
-            if type(self.table) == np.ndarray:
-                self.table[x][y] = self.__calculate(
-                    target, compared, x - 1, y - 1)
-            self.__calculate(target, compared, x - 1, y)
-            self.__calculate(target, compared, x, y - 1)
-            return self.__calculate(target, compared, x - 1, y - 1)
+        if len(s2) == 0:
+            step += len(s1)
+            return step
+
+        if s1[0] == s2[0]:
+            return self.evaluate(s1[1:], s2[1:], step)
         else:
-            if type(self.table) == np.ndarray:
-                self.table[x][y] = self.__minimize(self.__calculate(target, compared, x - 1, y), self.__calculate(
-                    target, compared, x, y - 1), self.__calculate(target, compared, x - 1, y - 1))+1
-            return self.__minimize(self.__calculate(target, compared, x - 1, y), self.__calculate(target, compared, x, y - 1), self.__calculate(target, compared, x - 1, y - 1)) + 1
-    
-    def evaluate(self, target: str, compared: str) -> int:
-        self.__init_table(target, compared)
-        len1 = len(target)
-        len2 = len(compared)
-        return self.__calculate(target, compared, len1, len2)
+            step += 1
+            return self.__minimize(self.evaluate(s1[1:], s2[:], step),
+                                   self.evaluate(s1[:], s2[1:], step),
+                                   self.evaluate(s1[1:], s2[1:], step))
 
 def main():
     edit_distinct = EditDistinct()
